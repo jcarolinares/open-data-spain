f22fbc13b1f6de5ec6aa85372b7dea85b2c0569b
jcarolinares <jcarolinares@gmail.com>
2018-05-02 20:24:58 +0200

    Testing pandas with invalid \scape problems

--- panda-test.py
+++ panda-test.py
@@ -13,9 +13,19 @@ import wget
 #df=pd.read_csv("http://datos.madrid.es/egob/catalogo/200967-0-mercados.csv")
 #df=pd.read_json("http://datos.madrid.es/egob/catalogo/200967-0-mercados.json")
 
+file=open('206974-0-agenda-eventos-culturales-100.json',"r")
+
+file_data=file.read()
+file.close()
+
+file=open('206974-0-agenda-eventos-culturales-100.json',"w")
+file_data=file_data.replace("\\","")
+file.write(file_data)
+file.close()
+
+
 data = json.load(open('206974-0-agenda-eventos-culturales-100.json'))
-
 df = pd.DataFrame(data["@graph"])
-
-
 print(df.head(5))
+
+#data = json.load(file_data)
